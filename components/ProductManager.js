4d1946cb8c07d80cf3b629afadf6303b662f1ab9
fati <faticarolinamelgarejo2@gmail.com>
2023-06-28 06:11:12 -0300

    cambio promesas

--- components/ProductManager.js
+++ components/ProductManager.js
@@ -1,157 +1,66 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 class ProductManager {
-  constructor(path) {
+  constructor() {
+    this.path = './product.json';
     this.products = [];
-    this.path = path;
   }
 
-  addProduct(product) {
-    return new Promise((resolve, reject) => {
-      if (
-        !product.title ||
-        !product.description ||
-        !product.price ||
-        !product.thumbnail ||
-        !product.code ||
-        !product.stock
-      ) {
-        return reject("Error: Todos los campos son obligatorios.");
-      }
+  static id = 0;
 
-      const existingProduct = this.products.find((p) => p.code === product.code);
-      if (existingProduct) {
-        return reject("Error: El código del producto ya existe.");
-      }
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    ProductManager.id++;
 
-      product.id = this.products.length + 1;
-      this.products.push(product);
+    let newProduct = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      id: ProductManager.id,
+    };
+    this.products.push(newProduct);
 
-      this.guardarProductos()
-        .then(() => {
-          resolve("Producto agregado correctamente.");
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
+    await fs.writeFile(this.path, JSON.stringify(this.products, null, 2) + '\n');
+  };
 
-  getProducts() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(this.path, 'utf8', (err, data) => {
-        if (err) {
-          console.error('Error al leer los productos:', err);
-          return reject(err);
-        }
-        if (data === '') {
-          return resolve([]);
-        }
-        const products = JSON.parse(data);
-        return resolve(products);
-      });
-    });
-  }
+  readProducts = async () => {
+    let prod = await fs.readFile(this.path, 'utf-8');
+    return JSON.parse(prod);
+  };
 
-  getProductById(id) {
-    return new Promise((resolve, reject) => {
-      this.getProducts()
-        .then((products) => {
-          const product = products.find((product) => product.id === id);
-          resolve(product);
-        })
-        .catch((e) => {
-          console.log('Error al obtener el producto:', e);
-          reject(e);
-        });
-    });
-  }
+  getProducts = async () => {
+    let res = await this.readProducts();
+    console.log(res);
+  };
 
-  updateProduct(id, updateProduct) {
-    return new Promise((resolve, reject) => {
-      this.getProducts()
-        .then((products) => {
-          const productIndex = products.findIndex((product) => product.id === id);
-
-          if (productIndex === -1) {
-            return reject();
-          }
-
-          products[productIndex].title = updateProduct.title;
-          products[productIndex].description = updateProduct.description;
-          products[productIndex].price = updateProduct.price;
-          products[productIndex].thumbnail = updateProduct.thumbnail;
-          products[productIndex].code = updateProduct.code;
-          products[productIndex].stock = updateProduct.stock;
-
-          this.guardarProductos()
-            .then(() => {
-              resolve('Producto actualizado');
-            })
-            .catch((err) => {
-              reject(err);
-            });
-        })
-        .catch((e) => {
-          console.log('Error al obtener el producto:', e);
-          reject(e);
-        });
-    });
-  }
-
-  deleteProduct(id) {
-    return new Promise((resolve, reject) => {
-      const productIndex = this.products.findIndex((p) => p.id === id);
-      if (productIndex === -1) {
-        return reject('Producto no encontrado');
-      }
-
-      this.products.splice(productIndex, 1);
-
-      this.guardarProductos()
-        .then(() => {
-          resolve('Producto eliminado');
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  }
-
-  leerProductos() {
-    try {
-      const data = fs.readFileSync(this.path, 'utf-8');
-      if (data === '') {
-        console.log(data);
-        return [];
-      }
-      return JSON.parse(data);
-    } catch (err) {
-      console.error('Error al leer los productos:', err);
-      return [];
+  getProductById = async (id) => {
+    let devolver = await this.readProducts();
+    if (!devolver.find((p) => p.id === id)) {
+      console.log('Producto no encontrado');
+    } else {
+      console.log(devolver.find((p) => p.id === id));
     }
-  }
+  };
 
-  guardarProductos() {
-    return new Promise((resolve, reject) => {
-      try {
-        const data = JSON.stringify(this.products, null, 2);
-        fs.writeFile(this.path, data, 'utf-8', (err) => {
-          if (err) {
-            console.log('Error al guardar los productos');
-            reject(err);
-          } else {
-            console.log('Productos guardados correctamente');
-            resolve();
-          }
-        });
-      } catch (err) {
-        console.log('Error al guardar los productos');
-        reject(err);
-      }
-    });
-  }
+  deleteProductById = async (id) => {
+    let eliminar = await this.readProducts();
+    let productFilter = eliminar.filter((p) => p.id != id);
+    await fs.writeFile(this.path, JSON.stringify(productFilter, null, 2) + '\n');
+    console.log('Producto Eliminado');
+  };
+
+  updateProduct = async ({ id, ...product }) => {
+    await this.deleteProductById(id);
+    let productOld = await this.readProducts();
+    let productReemplazado = [
+      { id, ...product },
+      ...productOld,
+    ];
+    await fs.writeFile(this.path, JSON.stringify(productReemplazado, null, 2) + '\n');
+  };
 }
 
-module.exports = ProductManager;
+module.exports = ProductManager
 
